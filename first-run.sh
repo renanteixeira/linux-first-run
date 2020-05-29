e851b75d89d0654ba3502c7c22f1776b36fc2551
Renan Teixeira <git@renanteixeira.com.br>
2020-05-29 20:50:19 -0300

    Add Colors

--- first-run.sh
+++ first-run.sh
@@ -1,29 +1,58 @@
 #!/bin/bash
-echo "Upload system"
+center() {
+	termwidth="$(tput cols)"
+	padding="$(printf '%0.1s' ={1..500})"
+	printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+echo -e "\e[1;100;92m"
+center "Update System"
+echo -e "\e[0m"
 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt autoremove -y
+
+echo -e "\e[1;100;92m"
+center "Install WGET, ZSH, GIT"
+echo -e "\e[0m"
+
 sudo apt-get install -y wget zsh git
 
 cd ~/
-echo "Download custom aliases"
+
+echo -e "\e[1;102;90m"
+center "Download custom aliases"
+echo -e "\e[0m"
+
 wget https://raw.githubusercontent.com/renanteixeira/linux-first-run/master/.aliases
 
-echo "Download Oh My ZSH"
+echo -e "\e[1;102;90m"
+center "Download Oh My Zsh"
+echo -e "\e[0m"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-echo "Download Zinit Plugin Manager"
+echo -e "\e[1;102;90m"
+center "Download Zinit Plugin Manager"
+echo -e "\e[0m"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)" "" --unattended
 
-echo "Set ZSH as default"
+echo -e "\e[1;46;37m"
+center "Set ZSH as default"
+echo -e "\e[0m"
+
 cat <<EOT >> ~/.bash_profile
 export SHELL=/bin/zsh
 exec /bin/zsh -l
 EOT
 
-echo "Edit .zshrc"
+echo -e "\e[1;46;37m"
+center "Edit .zshrc"
+echo -e "\e[0m"
+
 cat <<EOT >> ~/.zshrc
 zinit light zsh-users/zsh-autosuggestions
 zinit light zdharma/fast-syntax-highlighting
@@ -50,16 +79,22 @@ SPACESHIP_CHAR_SUFFIX=" "
 source $HOME/.aliases
 EOT
 
-echo "Download spaceship theme"
+echo -e "\e[1;102;90m"
+center "Download spaceship theme"
+echo -e "\e[0m"
 
 SPACESHIP_DIR="$HOME/.oh-my-zsh/custom/themes/spaceship-prompt"
 git clone https://github.com/denysdovhan/spaceship-prompt.git $SPACESHIP_DIR
 
-if [ -d "SPACESHIP_DIR/" ]
+if [ -d "$SPACESHIP_DIR/" ]
 then
-  ln -s "$SPACESHIP_DIR/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
-  sed -i -- 's/robbyrussell/spaceship/g' ~/.zshrc
+	ln -s "$SPACESHIP_DIR/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
+	sed -i -- 's/robbyrussell/spaceship/g' ~/.zshrc
 fi
 
-echo "Processo finalizado"
+echo -e "\e[1;103;90m"
+center "Process completed"
+center "Switching to ZSH and downloading settings"
+echo -e "\e[0m"
+
 zsh
